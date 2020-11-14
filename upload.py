c697ae3f86c48f176579fc6e3a16bc4cf13b1fc7
Aleksandra Górska <56221306+Nim98@users.noreply.github.com>
2020-11-14 21:19:20 +0100

    Upload

--- upload.py
+++ upload.py
@@ -5,54 +5,31 @@ import pymysql
 def convertToBinary(filepath):
     with open(filepath, 'rb') as file:
        db_image = file.read()
-    return db_image
-
-#szukanie indeksów w nazwie pliku dla znaków '_'
-def findIndexes(name, character):
-    indexes = []
-    for i, n in enumerate(name):
-        if n==character:
-            indexes.append(i)
-    return indexes
-
-#zmiana języka
-def changeLanguage(x):
-    return {
-        'Left': 'lewa',
-        'Right': 'prawa',
-        'index': 'wskazujący',
-        'little': 'mały',
-        'middle': 'środkowy',
-        'ring': 'serdeczny',
-        'thumb': 'kciuk',       
-    }[x]            
+    return db_image         
 
 #wyciąganie danych z nazwy pliku
 def getData(filename):
-    indexes = findIndexes(filename, '_')
-    id_person = filename[1:indexes[0]]
-    hand = filename[indexes[2]+1:indexes[3]]
-    finger = filename[indexes[3]+1:indexes[4]]
-    hand = changeLanguage(hand)
-    finger = changeLanguage(finger)
-    print(id_person, hand, finger)
-    return id_person, hand, finger
+    id_person = filename[-8:-6]
+    #id_person = int(id_person)+10
+    nr = filename[-5]
+    print(id_person, nr)
+    return id_person, nr
 
 #wykonanie operacji zapisywania w bazie dla wszystkich plików
 def forAllFiles(): 
     for filepath in glob.iglob(filespath, recursive=True):
         connection = pymysql.connect(host="localhost", user="root", passwd="", database="fingerprint_data")
         cursor = connection.cursor()
-        save_sql = 'INSERT INTO fingerprint (id_person, hand, finger, image) VALUES (%s,%s,%s,%s)'
+        save_sql = 'INSERT INTO fingerprint (id_person, nr, image) VALUES (%s,%s,%s)'
         db_image = convertToBinary(filepath)
         cut = filepath.rfind("\\")
         filename = filepath[cut:]
-        id_person, hand, finger = getData(filename)
-        db_tuple = (id_person, hand, finger, db_image)
+        id_person, nr = getData(filename)
+        db_tuple = (id_person, nr, db_image)
         cursor.execute(save_sql, db_tuple)
         connection.commit()
 
 #scieżka do pliku
-filespath = "c:/Users/agors/Desktop/Studia/Podstawy biometrii/Projekt/Mój/Dane/*.png"
+filespath = "c:/Users/agors/Desktop/Studia/Podstawy biometrii/Projekt/Mój/Dane/DB2/*.png"
     
 forAllFiles()
